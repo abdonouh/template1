20d0608c5f27d27594314cf56f5f6c98e7d81b6f
abdo_nouh <abdosmith494@gmail.com>
2009-05-11 16:10:53 +0200

    lots of change

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,8 @@
-const header = document.querySelector(".header");
+var header = document.querySelector(".header");
 window.addEventListener("scroll", function () {
-  var header = document.querySelector(".header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+  header.classList.toggle("sticky", window.scrollY > 10);
 });
+//const backColor =  
+//if(header)
+    
+
